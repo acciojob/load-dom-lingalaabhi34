42cefad5a57e7b79f12450add544a1fec5792268
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-20 11:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",function(){
+  function addtext() {
+    const body = document.body;
+    body.innerHTML = "DOM load success";
+  }
+  addtext();
+});
